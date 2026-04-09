d21eb447a7166edc3ac895a13eaf15c6304089aa
Rainbonium <66752661+Rainbonium@users.noreply.github.com>
2026-04-09 15:16:04 -0700

    Localize Image Storage

--- restaurantSite/settings.py
+++ restaurantSite/settings.py
@@ -121,6 +121,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 APPEND_SLASH = True
 
+IMAGE_STORAGE = "/static/images/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
